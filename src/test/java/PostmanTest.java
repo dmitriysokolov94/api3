885b1d79fe2fa1f058842e23bb8527d5d86ec238
dmitriysokolov94 <ipsokolov2021@gmail.com>
2022-07-27 19:19:30 +0300

    new

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+
+    @Test
+    void shodPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("text") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("tex"))
+
+        ;
+    }
+}
